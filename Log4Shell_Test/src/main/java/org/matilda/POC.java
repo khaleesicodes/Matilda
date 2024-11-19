c22ad3ca22f600d174d762da12e47bcabbb0a8e1
khaleesi <contact@khaleesicodes.com>
2024-11-19 17:53:54 +0100

    Added Javadocs

--- Log4Shell_Test/src/main/java/org/matilda/POC.java
+++ Log4Shell_Test/src/main/java/org/matilda/POC.java
@@ -1,10 +1,11 @@
 package org.matilda;
 
-
+/**
+ * Class to start and run a server to mock the Log4Shell exploit
+ */
 public class POC {
     public static void main(String[] args) throws InterruptedException {
         ServerLog serverLog = new ServerLog();
         serverLog.logging();
-        System.out.println("Finished Test run");
     }
 }
--- Log4Shell_Test/src/main/java/org/matilda/ServerLog.java
+++ Log4Shell_Test/src/main/java/org/matilda/ServerLog.java
@@ -14,12 +14,18 @@ import java.util.concurrent.atomic.AtomicReference;
 
 import org.junit.jupiter.api.Assertions;
 
+/**
+ * Mock Server that listens for ldap call by log4j, which is the first step in the Log4Shell exploit
+ */
 public class ServerLog {
     public void logging() throws InterruptedException {
         Logger logger = LogManager.getLogger(ServerLog.class);
         AtomicInteger port = new AtomicInteger();
+        // AtomicBoolean to check if
         AtomicBoolean hasRead = new AtomicBoolean(false);
+        // Create latch in order to allow thread to wait for other operation before continuing
         CountDownLatch latch = new CountDownLatch(1);
+        // Create reference to get random port nu
         AtomicReference<ServerSocket> socketRef = new AtomicReference<>();
         Thread server = new Thread(() -> {
             try (ServerSocket serverSocket = new ServerSocket()) {
@@ -28,10 +34,10 @@ public class ServerLog {
                 int localPort = serverSocket.getLocalPort();
                 logger.error("start server on port {} address: {} ", localPort,
                         serverSocket.getInetAddress().getHostAddress());
-
                 port.set(localPort);
                 latch.countDown();
                 logger.error("starting to listen");
+                // checks if connection to socket was requested
                 try (Socket accept = serverSocket.accept()) {
                     logger.error("accept");
                     try (InputStream stream = accept.getInputStream()) {
@@ -45,7 +51,9 @@ public class ServerLog {
         });
         server.start();
         latch.await();
-        logger.error("${jndi:ldap://127.0.0.1:" + port +"/33f00f7a-70b9-4efc-8fcd-9cde1085e17d}");
+        // String to exploit Log4Shell with port that listens
+        logger.error("${jndi:ldap://127.0.0.1:" + port +"/matilda-poc}");
+        // checks if any execution of connection has been done by the Logger - indicater for exploitation of Log4Shell
         Assertions.assertFalse(hasRead.get(), "LogForShell was not blocked by matilda");
 
         logger.error("Matilda has successfully blocked log4shell");
